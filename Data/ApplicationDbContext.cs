9db00c0b28b113a8c714638c668c80216ad0a24d
Robert <robert.tarczi@gmail.com>
2021-03-17 20:26:56 +0100

    Entity Framework

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Data.Models;
+
+namespace WorldCities.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base() { }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        #region Methods
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Map Entity names to DB Table names
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<Country>().ToTable("Countries");
+        }
+
+        #endregion Methods
+        
+        #region Properties
+        
+        public DbSet<City> Cities { get; set; }
+        
+        public DbSet<Country> Countries { get; set; }
+        
+        #endregion Properties
+    }
+}
